1cc2c51a1cd0cce05d4c6d6387313cb7a83f7416
Ricardo Diaz <Ricardo.Diaz@endava.com>
2020-01-09 10:21:48 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [phrase, getPhrase] = useState({});
+
+  const queryAPI = async () => {
+    const result = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    getPhrase(result.data[0]);
+  }
+
+  useEffect(
+    () => {
+      queryAPI();
+    },[]
+  )
+  console.log(phrase)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <p>Hello</p>
+  )
 }
 
 export default App;
